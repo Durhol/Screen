5af8c82bda963133dc3c5a651da6f213fd878a1b
Durhol <kamyshnikovdmytro@gmail.com>
2025-02-19 00:36:53 +0200

    Svetyashke v2

--- Screen.py
+++ Screen.py
@@ -3,6 +3,7 @@
 from tkinter import *
 import keyboard
 from random import *
+from pygame import mixer
 
 """/////////////////////////////"""
 
@@ -13,31 +14,62 @@ keyboard.block_key('alt')
 
 """/////////////////////////////"""
 
+mixer.init()
 root = Tk()
 root.title("Screen")
 root.attributes("-fullscreen", True)
 root.protocol("WM_DELETE_WINDOW", lambda: None)
 png = PhotoImage(file="image.png")
+ImageWidth = png.width()
+ImageHeight = png.height()
+
 ScreenWidth = root.winfo_screenwidth()
 ScreenHeight = root.winfo_screenheight()
+pngs = []
 
 """/////////////////////////////"""
+
+mixer.music.load("audio.mp3")
+mixer.music.set_volume(1)
+mixer.music.play(-1)
 
 PNGScreen = Canvas(root, width=ScreenWidth, height=ScreenHeight)
 PNGScreen.pack()
 PNG = PNGScreen.create_image(ScreenWidth // 2, ScreenHeight // 2, image=png)
+pngs.append(PNG)
 
 """/////////////////////////////"""
+def Duplicate():
+    global pngs
+    new_pngs = []
+    for original_png in list(pngs):
+        x, y = PNGScreen.coords(original_png)
+        if x and y:
+            NewPng = PNGScreen.create_image(x, y, image=png)
+            new_pngs.append(NewPng)
+    pngs.extend(new_pngs)
+    root.after(1400, Duplicate)
 
 def Moving():
-    mx = randint(-100, 100)
-    my = randint(-100, 100)
-    PNGScreen.move(PNG, mx, my)
-    root.after(100, Moving)
+    for png_item in list(pngs): 
+        coords = PNGScreen.coords(png_item)
+        x, y = coords
+
+        mx = randint(-100, 100)
+        my = randint(-100, 100)
+
+        neWx = min(max(x + mx, ImageWidth // 2), ScreenWidth - ImageWidth // 2)
+        neWy = min(max(y + my, ImageHeight // 2), ScreenHeight - ImageHeight // 2)
+
+        PNGScreen.coords(png_item, neWx, neWy)
+
+            
+    root.after(100, Moving)  
 
 """/////////////////////////////"""
 
 Moving()
+root.after(3000, Duplicate)
 root.mainloop()
 
  
